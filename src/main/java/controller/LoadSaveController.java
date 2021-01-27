8889fbaa29eadac08bdd5bba9c0bb374803dcc03
Piotr Tekielak <tekielak@student.agh.edu.pl>
2021-01-27 22:12:24 +0100

    Add saves list to load save view

--- src/main/java/controller/LoadSaveController.java
+++ src/main/java/controller/LoadSaveController.java
@@ -1,12 +1,20 @@
 package controller;
 
 import com.google.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
+import java.io.File;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoadSaveController extends Controller
 {
+    @FXML
+    private VBox savesBox;
+
     @Inject
     public LoadSaveController()
     {
@@ -15,5 +23,18 @@ public class LoadSaveController extends Controller
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        File folder = new File("saveGames");
+        for (final File fileEntry : Objects.requireNonNull(folder.listFiles()))
+        {
+            Button button = new Button(fileEntry.getName());
+            button.setOnAction(event -> mainController.loadGame(fileEntry));
+            savesBox.getChildren().add(button);
+        }
+    }
+
+    @FXML
+    public void handleBack()
+    {
+        mainController.openStartView();
     }
 }
--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -8,16 +8,16 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 @Singleton
 public class MainController implements Initializable
 {
-    private Injector injector;
+    private final Injector injector;
 
-    private Stage stage;
+    private final Stage stage;
 
     public MainController(Injector injector, Stage primaryStage)
     {
@@ -25,19 +25,9 @@ public class MainController implements Initializable
         this.stage = primaryStage;
     }
 
-    public void initRootLayout() throws IOException
+    public void initRootLayout()
     {
-        URL location = MainController.class.getResource("../view/StartView.fxml");
-        FXMLLoader fxmlLoader = new FXMLLoader(location);
-
-        fxmlLoader.setControllerFactory(injector::getInstance);
-
-        BorderPane rootLayout = fxmlLoader.load();
-
-        ((Controller) fxmlLoader.getController()).setMainController(this);
-
-        // add layout to a scene and show them all
-        configureStage(stage, rootLayout);
+        openStartView();
         stage.show();
     }
 
@@ -52,12 +42,10 @@ public class MainController implements Initializable
         {
             URL location = MainController.class.getResource("../view/" + name + ".fxml");
             FXMLLoader fxmlLoader = new FXMLLoader(location);
+            fxmlLoader.setControllerFactory(injector::getInstance);
             BorderPane root = fxmlLoader.load();
+            ((Controller) fxmlLoader.getController()).setMainController(this);
             configureStage(stage, root);
-//            Stage stage = new Stage();
-//            stage.setScene(new Scene(root));
-//            stage.show();
-            //main.stg.close();
         }
         catch(Exception e)
         {
@@ -70,12 +58,24 @@ public class MainController implements Initializable
         changeStage("LoadSaveView");
     }
 
+    public void openStartView()
+    {
+        changeStage("StartView");
+    }
+
     public void configureStage(Stage primaryStage, BorderPane rootLayout)
     {
-        var scene = new Scene(rootLayout);
+        rootLayout.setMinWidth(800);
+        rootLayout.setMinHeight(600);
+        Scene scene = new Scene(rootLayout);
         primaryStage.setScene(scene);
         primaryStage.setTitle("The Isle of Null");
         primaryStage.minWidthProperty().bind(rootLayout.minWidthProperty());
         primaryStage.minHeightProperty().bind(rootLayout.minHeightProperty());
+    }
+
+    public void loadGame(File saveFile)
+    {
+        System.out.println(saveFile);
     }
 }
